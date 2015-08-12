0313ac2e854bc9948a1c4fbedd18563c3242c0ae
nmby <nmby@hotchpotch.xyz>
2015-08-13 00:13:38 +0900

    ConsoleScanner宗旨替えして実装完了

--- project/src/main/java/xyz/hotchpotch/util/ConsoleScanner.java
+++ project/src/main/java/xyz/hotchpotch/util/ConsoleScanner.java
@@ -1,64 +1,159 @@
 package xyz.hotchpotch.util;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
 import java.util.Scanner;
+import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
 import java.util.regex.Pattern;
 
-public class ConsoleScanner implements Supplier<String> {
+public class ConsoleScanner<T> implements Supplier<T> {
     
     // ++++++++++++++++ static members ++++++++++++++++
     
-    private static final String DEFAULT_PROMPT = "> ";
-    private static final String DEFAULT_MSG = "入力形式が不正です。";
+    private static final String BR = System.lineSeparator();
+    
+    public static class Builder<T> {
+        private final Predicate<String> judge;
+        private final Function<String, T> converter;
+        private String prompt;
+        private String rePrompt;
+        
+        private Builder(
+                Predicate<String> judge,
+                Function<String, T> converter,
+                String prompt,
+                String rePrompt) {
+                
+            this.judge = judge;
+            this.converter = converter;
+            this.prompt = prompt;
+            this.rePrompt = rePrompt;
+        }
+        
+        public Builder<T> prompt(String prompt) {
+            this.prompt = Objects.requireNonNull(prompt);
+            return this;
+        }
+        
+        public Builder<T> rePrompt(String rePrompt) {
+            this.rePrompt = Objects.requireNonNull(rePrompt);
+            return this;
+        }
+        
+        public ConsoleScanner<T> build() {
+            return new ConsoleScanner<T>(judge, converter, prompt, rePrompt);
+        }
+    }
+    
+    public static Builder<String> stringBuilder(Predicate<String> judge) {
+        Objects.requireNonNull(judge);
+        return new Builder<String>(
+                judge,
+                Function.identity(),
+                "> ",
+                "入力形式が不正です > ");
+    }
+    
+    public static Builder<String> stringBuilder(Pattern pattern) {
+        Objects.requireNonNull(pattern);
+        return stringBuilder(pattern.asPredicate());
+    }
+    
+    public static Builder<String> stringBuilder(String regex) {
+        Objects.requireNonNull(regex);
+        return stringBuilder(Pattern.compile(regex));
+    }
+    
+    public static Builder<Integer> intBuilder(int lower, int upper) {
+        if (upper < lower) {
+            throw new IllegalArgumentException(String.format("lower=%d, upper=%d", lower, upper));
+        }
+        Predicate<String> judge = s -> {
+            // ちょっと乱暴だけど楽チンなので許して...
+            try {
+                Integer.valueOf(s);
+                return true;
+            } catch (NumberFormatException e) {
+                return false;
+            }
+        };
+        Function<String, Integer> converter = Integer::valueOf;
+        String prompt = String.format("%d～%dの範囲で指定してください > ", lower, upper);
+        return new Builder<Integer>(judge, converter, prompt, prompt);
+    }
+    
+    public static Builder<Long> longBuilder(long lower, long upper) {
+        if (upper < lower) {
+            throw new IllegalArgumentException(String.format("lower=%d, upper=%d", lower, upper));
+        }
+        Predicate<String> judge = s -> {
+            // ちょっと乱暴だけど楽チンなので許して...
+            try {
+                Long.valueOf(s);
+                return true;
+            } catch (NumberFormatException e) {
+                return false;
+            }
+        };
+        Function<String, Long> converter = Long::valueOf;
+        String prompt = String.format("%d～%dの範囲で指定してください > ", lower, upper);
+        return new Builder<Long>(judge, converter, prompt, prompt);
+    }
+    
+    public static <T> Builder<T> listBuilder(List<T> list) {
+        Objects.requireNonNull(list);
+        Predicate<String> judge = s -> {
+            // ちょっと乱暴だけど楽チンなので許して...
+            try {
+                int idx = Integer.parseInt(s);
+                return 0 <= idx && idx < list.size();
+            } catch (NumberFormatException e) {
+                return false;
+            }
+        };
+        Function<String, T> converter = s -> {
+            int idx = Integer.parseInt(s);
+            return list.get(idx);
+        };
+        StringBuilder tmp = new StringBuilder();
+        tmp.append("次の中から番号で指定してください。").append(BR);
+        for (int i = 0; i < list.size(); i++) {
+            tmp.append(String.format("\t%d : %s", i, list.get(i))).append(BR);
+        }
+        tmp.append("> ");
+        String prompt = tmp.toString();
+        return new Builder<T>(judge, converter, prompt, prompt);
+    }
+    
+    public static <E extends Enum<E>> Builder<E> enumBuilder(Class<E> type) {
+        Objects.requireNonNull(type);
+        return listBuilder(Arrays.asList(type.getEnumConstants()));
+    }
     
     // ++++++++++++++++ instance members ++++++++++++++++
     
     private final Predicate<String> judge;
+    private final Function<String, T> converter;
     private final String prompt;
     private final String rePrompt;
     
-    public ConsoleScanner(Predicate<String> judge, String prompt, String rePrompt) {
-        this.judge = Objects.requireNonNull(judge);
-        this.prompt = Objects.requireNonNull(prompt);
-        this.rePrompt = Objects.requireNonNull(rePrompt);
-    }
-    
-    public ConsoleScanner(Predicate<String> judge, String prompt) {
-        this(judge, prompt, DEFAULT_MSG + prompt);
-    }
-    
-    public ConsoleScanner(Predicate<String> judge) {
-        this(judge, DEFAULT_PROMPT);
-    }
-    
-    public ConsoleScanner(Pattern pattern, String prompt, String rePrompt) {
-        this(pattern.asPredicate(), prompt, rePrompt);
-    }
-    
-    public ConsoleScanner(Pattern pattern, String prompt) {
-        this(pattern.asPredicate(), prompt);
-    }
-    
-    public ConsoleScanner(Pattern pattern) {
-        this(pattern.asPredicate());
-    }
-    
-    public ConsoleScanner(String regex, String prompt, String rePrompt) {
-        this(Pattern.compile(regex), prompt, rePrompt);
-    }
-    
-    public ConsoleScanner(String regex, String prompt) {
-        this(Pattern.compile(regex), prompt);
-    }
-    
-    public ConsoleScanner(String regex) {
-        this(Pattern.compile(regex));
+    private ConsoleScanner(
+            Predicate<String> judge,
+            Function<String, T> converter,
+            String prompt,
+            String rePrompt) {
+            
+        this.judge = judge;
+        this.converter = converter;
+        this.prompt = prompt;
+        this.rePrompt = rePrompt;
     }
     
     @Override
-    public String get() {
+    public T get() {
         try (Scanner sc = new Scanner(System.in)) {
             System.out.print(prompt);
             String str = sc.nextLine();
@@ -66,7 +161,7 @@ public class ConsoleScanner implements Supplier<String> {
                 System.out.print(rePrompt);
                 str = sc.nextLine();
             }
-            return str;
+            return converter.apply(str);
         }
     }
 }
