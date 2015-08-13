7dfff38588caf0eb711ea9d5416b1d42610bd3a7
nmby <nmby@hotchpotch.xyz>
2015-08-13 20:17:29 +0900

    Rule仕上げ

--- project/src/main/java/xyz/hotchpotch/game/reversi/core/Rule.java
+++ project/src/main/java/xyz/hotchpotch/game/reversi/core/Rule.java
@@ -18,8 +18,8 @@ public class Rule {
      * ゲームが継続中か（黒か白の少なくともどちらかが駒を置ける場所があるか）を返します。<br>
      * 
      * @param board リバーシ盤
-     * @return ゲーム継続中の場合は true、どちらの色も置けず終了状態の場合は false
-     * @NullPointerException board が null の場合
+     * @return ゲーム継続中の場合は {@code true}、どちらの色も置けず終了状態の場合は {@code false}
+     * @NullPointerException {@code board} が {@code null} の場合
      */
     public static boolean isGameOngoing(Board board) {
         Objects.requireNonNull(board);
@@ -31,8 +31,8 @@ public class Rule {
      * 
      * @param board リバーシ盤
      * @param color 置く駒の色
-     * @return いずれかの位置に駒を置ける場合は true
-     * @NullPointerException board, color のいずれかが null の場合
+     * @return いずれかの位置に駒を置ける場合は {@code true}
+     * @NullPointerException {@code board}, {@code color} のいずれかが {@code null} の場合
      */
     public static boolean canPut(Board board, Color color) {
         Objects.requireNonNull(board);
@@ -47,8 +47,8 @@ public class Rule {
      * @param board リバーシ盤
      * @param color 置く駒の色
      * @param point 駒を置く位置
-     * @return 駒を置ける場合は true
-     * @NullPointerException board, color, point のいずれかが null の場合
+     * @return 駒を置ける場合は {@code true}
+     * @NullPointerException {@code board}, {@code color}, {@code point} のいずれかが {@code null} の場合
      */
     public static boolean canPutAt(Board board, Color color, Point point) {
         Objects.requireNonNull(board);
@@ -66,8 +66,8 @@ public class Rule {
      * 
      * @param board リバーシ盤
      * @param move 手
-     * @return 手を適用できる場合は true
-     * @NullPointerException board, move のいずれかが null の場合
+     * @return 手を適用できる場合は {@code true}
+     * @NullPointerException {@code board}, {@code move} のいずれかが {@code null} の場合
      */
     public static boolean canApply(Board board, Move move) {
         Objects.requireNonNull(board);
@@ -88,15 +88,15 @@ public class Rule {
         assert board.colorAt(point) == null;
         
         int n = 0;
-        Point p = point.next(direction);
-        while (p != null) {
+        Point p = point;
+        while (p.hasNext(direction)) {
+            p = p.next(direction);
             if (board.colorAt(p) == color.opposite()) {
                 return n;
             } else if (board.colorAt(p) == null) {
                 return 0;
             }
             n++;
-            p = p.next(direction);
         }
         return 0;
     }
@@ -104,37 +104,32 @@ public class Rule {
     /**
      * リバーシ盤上に指定された手を適用した場合に、
      * それぞれの方向に相手の駒を何枚ひっくり返せるかを返します。<br>
-     * 置くことのできない手が指定された場合は、0 のみが格納されたマップを返します。<br>
+     * 置くことのできない手が指定された場合は、{@code 0} のみが格納されたマップを返します。<br>
      * 
      * @param board リバーシ盤
      * @param move 手
-     * @return 方向ごとにひっくり返せる枚数を格納した Map
-     * @NullPointerException board, move のいずれかが null の場合
+     * @return 方向ごとにひっくり返せる枚数を格納した {@code Map}
+     * @NullPointerException {@code board}, {@code move} のいずれかが {@code null} の場合
      */
     public static Map<Direction, Integer> counts(Board board, Move move) {
         Objects.requireNonNull(board);
         Objects.requireNonNull(move);
         
         Map<Direction, Integer> counts = new EnumMap<>(Direction.class);
-        
-        // Direction#stream() が並列ストリームを生成する可能性を考慮して、Collections#synchronizedMap でラップする。
-        // （この考え方で正しいんやろか...？）
-        Map<Direction, Integer> wrappedCounts = Collections.synchronizedMap(counts);
+        Map<Direction, Integer> wrapped = Collections.synchronizedMap(counts);
         Direction.stream().forEach(
-                d -> wrappedCounts.put(d, countReversibles(board, move.color, move.point, d)));
-        
-        // ラップしたまま返すのもなんなので、中身だけを返す。
-        // （この考え方で正しいんやろか...？）
+                d -> wrapped.put(d, countReversibles(board, move.color, move.point, d)));
+                
         return counts;
     }
     
     /**
      * 勝者の色を返します。
-     * 引き分けの場合は null を返します。<br>
+     * 引き分けの場合は {@code null} を返します。<br>
      * 
      * @param board リバーシ盤
-     * @return 勝者の色（引き分けの場合は null）
-     * @throws NullPointerException board が null の場合
+     * @return 勝者の色（引き分けの場合は {@code null}）
+     * @throws NullPointerException {@code board} が {@code null} の場合
      * @throws IllegalStateException ゲーム継続中の場合
      */
     public static Color winner(Board board) {
