aefa0ee618cef9c95eb27bafd9b5e79c32f9b971
nmby <nmby@hotchpotch.xyz>
2015-08-14 02:22:02 +0900

    Result, GameResult仕上げ

--- project/src/main/java/xyz/hotchpotch/game/reversi/framework/GameResult.java
+++ project/src/main/java/xyz/hotchpotch/game/reversi/framework/GameResult.java
@@ -6,76 +6,88 @@ import java.util.Objects;
 import xyz.hotchpotch.game.reversi.core.Board;
 import xyz.hotchpotch.game.reversi.core.BoardSnapshot;
 import xyz.hotchpotch.game.reversi.core.Color;
-import xyz.hotchpotch.game.reversi.core.RuleViolationException;
 
-public class GameResult {
+/**
+ * ゲームの結果を表す不変クラスです。<br>
+ * 
+ * @author nmby
+ */
+public class GameResult implements Result<Game> {
     
     // ++++++++++++++++ static members ++++++++++++++++
     
-    private static final String BR = System.lineSeparator();
-    
+    /**
+     * ルール違反なくゲームが正常終了した場合のゲーム結果を生成します。<br>
+     * 
+     * @param gameCondition ゲーム条件
+     * @param board ゲーム終了時のリバーシ盤
+     * @return ゲーム結果
+     * @throws NullPointerException {@code gameCondition}、{@code board} のいずれかが {@code null} の場合
+     */
     public static GameResult of(GameCondition gameCondition, Board board) {
         Objects.requireNonNull(gameCondition);
         Objects.requireNonNull(board);
-        return null;
+        return new GameResult(gameCondition, board);
     }
     
+    /**
+     * ルール違反によりゲームが終了した場合のゲーム結果を生成します。<br>
+     * 
+     * @param gameCondition ゲーム条件
+     * @param violation ルール違反
+     * @return ゲーム結果
+     * @throws NullPointerException {@code gameCondition}、{@code violation} のいずれかが {@code null} の場合
+     */
     public static GameResult of(GameCondition gameCondition, RuleViolationException violation) {
         Objects.requireNonNull(gameCondition);
         Objects.requireNonNull(violation);
-        return null;
+        return new GameResult(gameCondition, violation);
     }
     
     // ++++++++++++++++ instance members ++++++++++++++++
     
     public final GameCondition gameCondition;
     public final Board board;
-    public final Color gainer;
     public final RuleViolationException violation;
+    public final Color winner;
+    private final String description;
     
     private GameResult(GameCondition gameCondition, Board board) {
         this.gameCondition = gameCondition;
         this.board = BoardSnapshot.of(board);
+        this.violation = null;
+        
         Map<Color, Integer> counts = this.board.counts();
         int black = counts.get(Color.BLACK);
         int white = counts.get(Color.WHITE);
         if (white < black) {
-            gainer = Color.BLACK;
+            winner = Color.BLACK;
         } else if (black < white) {
-            gainer = Color.WHITE;
+            winner = Color.WHITE;
         } else {
-            gainer = null;
+            winner = null;
         }
-        violation = null;
+        
+        description = String.format("%s%s:%d, %s:%d",
+                winner == null ? "引き分けです。" : winner + "の勝ちです。",
+                Color.BLACK, black, Color.WHITE, white);
     }
     
     private GameResult(GameCondition gameCondition, RuleViolationException violation) {
         this.gameCondition = gameCondition;
         this.violation = violation;
-        gainer = violation.violator.opposite();
         board = null;
+        winner = violation.violator.opposite();
+        description = String.format("%sの反則負けです。%s", violation.violator, violation.getMessage());
     }
     
+    /**
+     * このゲーム結果の文字列表現を返します。<br>
+     * 
+     * @return このゲーム結果の文字列表現
+     */
     @Override
     public String toString() {
-        StringBuilder str = new StringBuilder();
-        
-        if (gainer == null) {
-            str.append("引き分けです。").append(BR);
-        } else {
-            str.append(String.format("%sの勝ちです。", gainer)).append(BR);
-        }
-        
-        if (violation == null) {
-            Map<Color, Integer> counts = board.counts();
-            int black = counts.get(Color.BLACK);
-            int white = counts.get(Color.WHITE);
-            str.append(String.format("%s : %d, %s : %d", Color.BLACK, black, Color.WHITE, white));
-        } else {
-            str.append(String.format("%sがルールに違反しました。", violation.violator)).append(BR);
-            str.append(violation.getMessage());
-        }
-        
-        return str.toString();
+        return description;
     }
 }
---
+++ project/src/main/java/xyz/hotchpotch/game/reversi/framework/Result.java
@@ -0,0 +1,10 @@
+package xyz.hotchpotch.game.reversi.framework;
+
+/**
+ * ゲーム等の実行結果を表します。<br>
+ * 
+ * @param <P> ターゲット {@code Playable}
+ * @author nmby
+ */
+public interface Result<P extends Playable> {
+}
