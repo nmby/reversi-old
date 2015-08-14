56db450a5758582e3cf8755f7a2557cd647ff2f9
nmby <nmby@hotchpotch.xyz>
2015-08-14 19:13:02 +0900

    修正

--- project/src/main/java/xyz/hotchpotch/game/reversi/framework/League.java
+++ project/src/main/java/xyz/hotchpotch/game/reversi/framework/League.java
@@ -1,5 +1,7 @@
 package xyz.hotchpotch.game.reversi.framework;
 
+import java.util.Objects;
+
 /**
  * 複数のプレーヤーが総当たりでそれぞれ複数回対戦する、リーグを表します。<br>
  * 複数のマッチを束ねたものです。<br>
@@ -9,4 +11,42 @@ package xyz.hotchpotch.game.reversi.framework;
  * @author nmby
  */
 public interface League extends Playable {
+    
+    // ++++++++++++++++ static members ++++++++++++++++
+    
+    /**
+     * リーグにおける個々のマッチの組み合わせを表すための不変クラスです。<br>
+     */
+    static class Pair {
+        final int idx1;
+        final int idx2;
+        
+        Pair(int idx1, int idx2) {
+            this.idx1 = idx1;
+            this.idx2 = idx2;
+        }
+        
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public boolean equals(Object o) {
+            if (o instanceof Pair) {
+                Pair other = (Pair) o;
+                return idx1 == other.idx1 && idx2 == other.idx2;
+            }
+            return false;
+        }
+        
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public int hashCode() {
+            return Objects.hash(idx1, idx2);
+        }
+    }
+    
+    // ++++++++++++++++ instance members ++++++++++++++++
+    
 }
