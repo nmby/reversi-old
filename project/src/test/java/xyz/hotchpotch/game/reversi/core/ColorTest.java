8cf23e07e5255dc7e9d05aaffd0ccc915e65f08a
nmby <nmby@hotchpotch.xyz>
2015-08-22 22:13:19 +0900

    テストコード作成

---
+++ project/src/test/java/xyz/hotchpotch/game/reversi/core/ColorTest.java
@@ -0,0 +1,56 @@
+package xyz.hotchpotch.game.reversi.core;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ColorTest {
+    
+    @Test
+    public void testValues() {
+        assertThat(Color.values().length, is(2));
+        
+        assertThat(Color.values()[0], theInstance(Color.BLACK));
+        assertThat(Color.values()[1], theInstance(Color.WHITE));
+    }
+    
+    @Test
+    public void testValueOf() {
+        assertThat(Color.valueOf("BLACK"), theInstance(Color.BLACK));
+        assertThat(Color.valueOf("WHITE"), theInstance(Color.WHITE));
+    }
+    
+    @Test
+    public void testStream() {
+        assertThat(Color.stream().isParallel(), is(false));
+        assertThat(Color.stream().count(), is(2L));
+        assertThat(Color.stream().toArray(Color[]::new), is(Color.values()));
+    }
+    
+    @Test
+    public void testParallelStream() {
+        assertThat(Color.parallelStream().isParallel(), is(true));
+        assertThat(Color.parallelStream().count(), is(2L));
+        assertThat(Color.parallelStream().toArray(Color[]::new), is(Color.values()));
+    }
+    
+    @Test
+    public void testToStringColor() {
+        assertThat(Color.toString(Color.BLACK), is(Color.BLACK.toString()));
+        assertThat(Color.toString(Color.WHITE), is(Color.WHITE.toString()));
+        assertThat(Color.toString(null), is("・"));
+    }
+    
+    @Test
+    public void testOpposite() {
+        assertThat(Color.BLACK.opposite(), theInstance(Color.WHITE));
+        assertThat(Color.WHITE.opposite(), theInstance(Color.BLACK));
+    }
+    
+    @Test
+    public void testToString() {
+        assertThat(Color.BLACK.toString(), is("●"));
+        assertThat(Color.WHITE.toString(), is("○"));
+    }
+}
