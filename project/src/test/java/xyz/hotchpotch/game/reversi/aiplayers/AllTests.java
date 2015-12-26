60902a94049c1e58e3daf90c98d168229401a8ba
nmby <nmby@hotchpotch.xyz>
2015-12-27 02:10:01 +0900

    テストスイート作成

---
+++ project/src/test/java/xyz/hotchpotch/game/reversi/aiplayers/AllTests.java
@@ -0,0 +1,13 @@
+package xyz.hotchpotch.game.reversi.aiplayers;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+        xyz.hotchpotch.game.reversi.aiplayers.AllTests.class,
+        xyz.hotchpotch.game.reversi.core.AllTests.class
+})
+public class AllTests {
+}
